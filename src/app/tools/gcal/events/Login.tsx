16d683ec0c7f812bb1bd9fbab219744368a9029e
Keegan Good <kgood@opb.org>
2025-05-19 19:29:39 -0700

    transfer

--- src/app/tools/gcal/events/Login.tsx
+++ src/app/tools/gcal/events/Login.tsx
@@ -2,9 +2,10 @@
 
 import React, { useState } from 'react'
 import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth';
-import { auth } from '@/app/firebase'
+import { auth, authProvider } from '@/app/firebase'
 import { SiGooglecalendar } from "react-icons/si";
 import { Box, Button, Container, Text, Heading } from '@chakra-ui/react';
+import { google } from 'googleapis';
 
 const Login = () => {
     const [user, setUser] = useState<User | null>(null)
@@ -15,28 +16,8 @@ const Login = () => {
         setLoading(true);
         setError(null);
         try {
-
-            const provider = new GoogleAuthProvider();
-            provider.addScope('https://www.googleapis.com/auth/calendar.events');
-            provider.setCustomParameters({
-                prompt: 'select_account',
-            })
-            const result = await signInWithPopup(auth, provider)
-            const accessToken = GoogleAuthProvider.credentialFromResult(result)?.accessToken;
-
-            const idToken = await result.user.getIdToken();
-            const refreshToken = result.user.refreshToken;
-
-            setUser(result.user)
-            await fetch('/api/auth', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${idToken}`
-                },
-                body: JSON.stringify({ accessToken, refreshToken }),
-                credentials: 'include',
-            })
+            const result = await signInWithPopup(auth, authProvider);
+            console.log({result})
 
         } catch (error: any) {
             //     setError(error);
